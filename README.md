0190ec18aaceb628ba56849586278a7bdbcc8802
syed Rizvi <rizvi5a@yahoo.com>
2021-04-02 19:35:20 -0500

    README.MD file is added

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # workday-scheduler
 # Starting Workday Scheduler Assignment
+# time clock is displayed under work-day-schedulr header
+# Bootstrap col-md-3, col-md-6 and col-md-3 are used
+# to display, time of the day, textarea and save buttons, respectively
+# Each <div class="row"> conatines time heading <h6> and spanned over 3-columns,
+# then each textarea is spanned over 6-columns and save button over rest of the 12-columns sapn.
+# each textarea has an id  in the form of "#-plan" ( from 9:00 am to 6:00 pm ), 
+# and 9-plan to 18-plan , respectively,.
+# Save buttons ids are from 9-btn to 18-btn.
+# Textarea "id" is split using Jquery "split method"
+# then a for loop is used to get stores messages 
+# clicking on save buttons on each time.
+
+
--- js/script.js
+++ js/script.js
@@ -1,19 +1,38 @@
+
+ var timeDisplayEl = $('#clock');
+ // handle displaying the time
+ function displayTime() {
+     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+     timeDisplayEl.text(rightNow);
+   };
+ 
+   setInterval(displayTime, 1000);
+ 
+// Id #-Plan is split into #  and -plan 
+// using JQuery on ("click", function())
+// and changing the " id" attribute of #-plan class
 $(".saveplan").on("click", function () {
     var time = $(this).attr("id").split("-")[0];
     var text = $("#" + time + "-plan").val()
+// time and text are stored in local storage 
     console.log("Timer", time, text)
     localStorage.setItem(time, text)
 })
+
+//current time is converted into 24:00 hours format
+// to use 9:000 to 18:000 hours to get stored text 
+// from local storage area and to chnage the background colors
+// using a for loop from 9 to 18
 var currentHour = moment().hours()
 
     for (var i = 9; i <= 18; i++) {
         console.log(currentHour, localStorage.getItem(i))
         $("#" + i + "-plan").val(localStorage.getItem(i))
         if(currentHour> i){
-            $("#" + i + "-plan").addClass("bg-primary")     
+            $("#" + i + "-plan").addClass("past")     
         }else if(currentHour == i){
-            $("#" + i + "-plan").addClass("bg-info")     
+            $("#" + i + "-plan").addClass("present")     
         }else{
-            $("#" + i + "-plan").addClass("bg-secondary")     
+            $("#" + i + "-plan").addClass("future")     
         }
     }
